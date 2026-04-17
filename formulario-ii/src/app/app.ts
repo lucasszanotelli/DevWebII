9445d6562e4e3e5419c4b0e7263291e16377b4df
lucas <lucasszanotelli@gmail.com>
2026-04-17 15:48:55 -0300

    form v

--- formulario-ii/src/app/app.ts
+++ formulario-ii/src/app/app.ts
@@ -3,11 +3,12 @@ import { Forms } from './form-i/forms';
 import { FormII } from './form-ii/form-ii';
 import { FormIII } from './form-iii/form-iii';
 import { FormIV } from './form-iv/form-iv';
+import { FormV } from './form-v/form-v';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Forms, FormII, FormIII, FormIV],
+  imports: [Forms, FormII, FormIII, FormIV, FormV],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- formulario-ii/src/app/form-v/form-v.ts
+++ formulario-ii/src/app/form-v/form-v.ts
@@ -1,6 +1,39 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidatorFn, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+
+
+function numeroPositivoValido(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const valor = String(control.value ?? '').trim();
+    if (!valor) return null;
+
+    if (!/^\d+$/.test(valor)) {
+      return { numeroInvalido: true };
+    }
+
+    const numero = Number(valor);
+    return numero <= 0 ? { numeroInvalido: true } : null;
+  };
+}
+
+function bairroNoLogradouro(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const logradouro = String(control.get('logradouro')?.value ?? '');
+    const bairro = String(control.get('bairro')?.value ?? '');
+
+    // Normalização
+    const logradouroNormalizado = logradouro.trim().toLowerCase();
+    const bairroNormalizado = bairro.trim().toLowerCase();
+
+    return logradouroNormalizado !== '' &&
+      bairroNormalizado !== '' &&
+      logradouroNormalizado.includes(bairroNormalizado)
+      ? { bairroNoLogradouro: true }
+      : null;
+
+  };
+}
 
 
 @Component({
@@ -12,12 +45,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 export class FormV {
   private fb = inject(FormBuilder);
   formulario = this.fb.group({
-    nome: [''],
-    endereco: this.fb.group({
-      logradouro: [''],
-      numero: [''],
-      bairro: ['']
-    })
+    nome: ['', [Validators.required, Validators.minLength(3)]],
+    endereco: this.fb.group(
+      {
+        logradouro: [''],
+        numero: ['', [Validators.required, numeroPositivoValido()]],
+        bairro: [''],
+      },
+      { validators: [bairroNoLogradouro()] },
+    ),
   });
 
 
@@ -35,6 +71,19 @@ export class FormV {
 
   limparTudo() {
     this.formulario.reset();
+  }
+
+  get nome() {
+    return this.formulario.get('nome');
+  }
+  get endereco() {
+    return this.formulario.get('endereco');
+  }
+  get numero() {
+    return this.endereco?.get('numero');
+  }
+  get bairro() {
+    return this.endereco?.get('bairro');
   }
 
 }
